8aa3f795cb6aaafcb782b119964c53211e64ca16
atoui79 <atoui79@gmail.com>
2025-01-04 15:48:39 -0600

    header shadow

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,10 @@
 function Header() {
     return (
         <>
-            <div className="header" style={{ backgroundColor: "white", maxHeight: "100px", minHeight:'50px', width: "100vw", placeItems: "center", display: "flex", padding: "10px 25px", color: 'black' }}>
+            <div className="header" style={{ boxShadow: '0 0 8px rgba(0, 0, 0, .65)', zIndex: '99990',backgroundColor: "var(--header-color)", maxHeight: "75px", minHeight:'50px', width: "100vw", placeItems: "center", display: "flex", padding: "10px 25px", color: 'var(--header-text)' }}>
                 <div className="header-content" style={{ maxWidth: "1280px", width: '100%', display: 'flex', justifyContent: 'space-between', margin: '0 auto' }}>
                     <h1 style={{}}>Test</h1>
-                    <div style={{ display: 'flex', placeItems: 'center', justifyContent: 'space-between', gap: '1rem'}}>
+                    <div style={{ display: 'flex', placeItems: 'center', justifyContent: 'space-between', gap: '1rem', fontFamily: 'Helvetica'}}>
                         <h2> Menu Item </h2>
                         <h2> Menu Item </h2>
                         <h2> Menu Item </h2>
