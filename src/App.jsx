104d86f67c7b8d6fe4bf2e3b63ee8b155fa7dfa1
sami <atoui79@gmail.com>
2025-01-21 17:37:16 -0600

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import {Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import Header from './Components/Header'
 import Contact from './Pages/Contact'
 import Home from './Pages/Home';
+import Footer from './Components/Footer';
 function App() {
 
   return (
@@ -10,10 +11,13 @@ function App() {
       <div>
         <Header />
       </div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="contact" element={<Contact />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+      <footer>
+        <Footer />
+      </footer>
     </>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,22 @@
+function Footer() {
+    return (
+        <>
+            <div className="footer-main">
+                <div className="footer-left">
+                    <h1>chaqradesigns.</h1>
+                    Luscious Orange is a creative agency specializing in design, strategy, and web development. Based in Winnipeg, Manitoba, our team works closely, collaboratively, and completely remotely with customers near and far.
+                </div>
+                <div className="footer-right">
+                    <h3>Get in Touch</h3>
+                    <p>(204)997-7121</p>
+                    <div>  SOCIAL ICONS </div>
+                </div>
+            </div>
+            <div style={{ backgroundColor:"black", color: "white", padding: "25px 10px"}}>
+                Copyright chaqradesigns. 2025
+            </div>
+        </>
+    )
+}
+
+export default Footer;
